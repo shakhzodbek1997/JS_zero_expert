9e6c4af787f66c6fda58230ad0236ba79539753d
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-10-15 14:22:35 +0200

    challenge #1 part 2

--- js_fundamental_part2.js
+++ js_fundamental_part2.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/**
 let hasDriversLicense = false;
 const passtest = false;
 if(passtest){
@@ -30,4 +30,26 @@ function calcAge(birthYear){
     return 2037 - birthYear;
 }
 const age = calcAge(1997);
-console.log(age);  
+console.log(age); 
+ */
+
+// Coding Challenge # part 2
+
+const calcAverage = (a, b, c) => (a+b+c)/3; // average score
+console.log(calcAverage(3, 4, 5))
+
+const scoreD = calcAverage(44, 23, 71);
+const scoreK = calcAverage(65, 54, 49);
+console.log(scoreD, scoreK);
+
+const checkWinner = function(averD, averK){
+    if(averD >= averK*2){
+        console.log(`Dolphins win trophy🏆 ${averD} vs ${averK}`);
+    }else if(averK >= averD * 2){
+        console.log(`Koalas win trophy🏆 ${averK} vs ${averD}`);
+    }else{
+        console.log("No team Wins...")
+    }
+}
+
+checkWinner(scoreD, scoreK);
