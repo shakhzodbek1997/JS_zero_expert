6d0dc43da48cc1c4059bd611bf340382d32bcdce
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-10-18 21:13:41 +0200

    part 2 fundamentals JS

--- js_fundamental_part2.js
+++ js_fundamental_part2.js
@@ -56,7 +56,8 @@ checkWinner(scoreD, scoreK);
  */
 
 //  --- arrays Coding Challenge #2
- 
+
+/**
 const calcTip = function(bill){
     return bill  >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
@@ -69,7 +70,7 @@ const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
 
 
-console.log(bills, tips, totals);
+console.log(bills, tips, totals);     */
 
 // const  calculateTip = bill => bill >= 50 && bill <=300 ? bill * 0.15 : bill * 0.2;
 
@@ -77,6 +78,8 @@ console.log(bills, tips, totals);
 /**
  Jonas has 3 friends, and his best friend is called Michael 
  */
+
+ /**
 
  const jonas = {
     firstName: "Jonas",
@@ -87,3 +90,85 @@ console.log(bills, tips, totals);
  }
 
  console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`)
+ */
+
+
+ // Coding Challenge # 3
+/**
+ const mark = {
+    fullName: "Mark Miller",
+    mass: 78,
+    height: 1.69,
+    calcBMI: function(){
+        this.bmi = this.mass/this.height ** 2;
+        return this.bmi;
+    }
+ }
+
+ const john = {
+    fullName: "John Smith",
+    mass: 92,
+    height: 1.95,
+    calcBMI: function(){
+        this.bmi = this.mass/this.height ** 2;
+        return this.bmi;
+    }
+ }
+
+ mark.calcBMI();
+ console.log(mark.bmi);
+
+ 
+ john.calcBMI();
+ console.log(john.bmi);
+
+ if(mark.bmi > john.bmi){
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higheer than ${john.fullName}'s BMI (${john.bmi})`)
+ }else if(john.bmi > mark.bmi){
+    console.log(`${john.fullName}'s BMI (${john.bmi}) is higheer than ${mark.fullName}'s BMI (${mark.bmi})`)
+ }else{
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) and ${john.fullName}'s BMI (${john.bmi}) are equal!`)
+ }
+  */
+
+ // LOOping Arrays
+
+//  const years = [1991, 1995, 2001, 2019];
+//  const ages = [];
+
+//  for(let i = 0; i < years.length; i++){
+//     ages.push(2022 -years[i]);
+//  }
+//  console.log(ages); 
+
+//  ---------- Fudamental JS part 2
+ const calcTip = function(bill){
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+ }
+
+ const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+ const tips = [];
+ const totals = [];
+
+ for(let i = 0; i < bills.length; i++){
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+ }
+ console.log(bills)
+ console.log("-----------------------")
+
+ console.log(tips)
+ console.log("-----------------------")
+
+ console.log(totals)
+ console.log("-----------------------")
+
+ console.log(bills, tips, totals)
+
+ const calcAverage = function(arr){
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        sum = sum + arr[i];
+    }
+ }
