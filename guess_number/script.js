9a4c285967d6a1f9fcc1dff89cafa30d9576afc7
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-10-22 13:59:11 +0200

    gess number app done

--- guess_number/script.js
+++ guess_number/script.js
@@ -11,13 +11,11 @@ console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
 
  */
-const secretNumber = Math.trunc(Math.random()*20) + 1;
+let secretNumber = Math.trunc(Math.random()*20) + 1;
 let  score = document.querySelector('.score').textContent = 20;
+let highScore = 0;
 
-
-document.querySelector('.number').textContent = "?";
-
-document.querySelector('.check') .addEventListener('click', function(){
+document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
     if(!guess){
         document.querySelector('.message').textContent = "⛔No Number";
@@ -26,6 +24,11 @@ document.querySelector('.check') .addEventListener('click', function(){
         document.querySelector('.message').textContent = "🎉 Correct Number!";
         document.querySelector('body').style.backgroundColor = "#60b347";
         document.querySelector('.number').style.width = "30rem";
+    
+        if(score > highScore){
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
     }else if(guess > secretNumber){
         if(score > 1){
             document.querySelector('.message').textContent = "📈 Too High!";
@@ -46,3 +49,18 @@ document.querySelector('.check') .addEventListener('click', function(){
         }
     }
 });
+
+document.querySelector('.again').addEventListener('click', function(){ 
+    score = 20;
+    secretNumber = Math.trunc(Math.random()*20) + 1;
+     
+    document.querySelector('.number').textContent = "?";
+    document.querySelector('.guess').value = "";
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.message').textContent = "Start guessing...";
+    document.querySelector('body').style.backgroundColor = "#222";
+    document.querySelector('.number').style.width = "15rem";
+})
+
+
+// refactoring our code The Dry Principle
