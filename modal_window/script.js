a6a3279bb73ab0efc7e01cd1a70914d280ea0fa3
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-10-22 19:34:34 +0200

    modal window is done

--- modal_window/script.js
+++ modal_window/script.js
@@ -1,3 +1,23 @@
 'use strict';
-console.log("Hello World!");
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClose = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal'); 
+
+const openModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for(let i = 0; i < btnsOpenModal.length; i++){
+     btnsOpenModal[i].addEventListener('click', openModal); 
+}
  
+btnClose.addEventListener('click', closeModal); 
+overlay.addEventListener('click', closeModal);
