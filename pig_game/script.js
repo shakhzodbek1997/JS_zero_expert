da075b8b0fb1446dc649d8103d8b5fec1e61d903
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-10-23 00:37:10 +0200

    Switching active player is done

--- pig_game/script.js
+++ pig_game/script.js
@@ -1,5 +1,7 @@
 'use script';
 // Selecting elements 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -15,8 +17,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0 ]
 let currentScore = 0;
-
+let activePlayer = 0; // player 
 
 // Rolling the Dice functionality
 btnRoll.addEventListener('click', function(){
@@ -29,8 +32,15 @@ btnRoll.addEventListener('click', function(){
     if(dice !== 1){
         // Add dice to the current Score
         currentScore += dice;
-        current0El.textContent = currentScore;  
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     }else{
-        // Switching to the next Player
+        // Switching to the next Player 
+        document.getElementById(`current--${activePlayer}`).textContent = 0
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
+
+
     }
 })
