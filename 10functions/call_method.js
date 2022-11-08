48ed99a67182b8b8f402606f289bd83d07d228ad
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-11-08 13:47:22 +0100

    BIND method

--- 10functions/call_method.js
+++ 10functions/call_method.js
@@ -28,7 +28,8 @@ const book = uzairways.book; // this book function is a separate function from t
 // book(23, 'Sara Williams');
 
 
-// ---------call method----------
+console.log('==================== CALL METHOD ====================');
+
 book.call(eurowings, 23, 'JIM CARREY');
 console.log(eurowings);
 
@@ -45,7 +46,7 @@ const swiss = {
 
 book.call(swiss, 583, 'DALE CARNEGIE');
 
-// -------Apply Method
+console.log('==================== Apply Method ====================');
 
 const flightData = [533, 'GEORGE COOPER'];
 book.apply(swiss, flightData); // THIS APPLY() METHOD IS OLD VERSION SQO IT DON'T USE ANYMORE
@@ -53,3 +54,51 @@ console.log(swiss);
 
 book.call(swiss, ...flightData);
 
+console.log("====================== BIND method ==================");
+const bookEW = book.bind(eurowings);
+const bookUZ = book.bind(uzairways);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'STEVEN WILLIAMS');  
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Shakhzod Yuldoshov');
+bookEW23('Doniyor Nabiev');
+
+
+console.log('================= Event Listeners ===================');
+
+uzairways.planes = 300; // this company has 300 planes
+uzairways.buyPlane = function(){
+    console.log(this);
+
+    this.planes++;
+    console.log("I am here: ", this.planes)
+}
+
+document.querySelector('.buy').addEventListener('click', uzairways.buyPlane.bind(uzairways));
+
+console.log('==================PARTIAL APPLICATION==================');
+
+const addTax = (rate, value) => value + (value * rate)/100; // this is tax adding ARROW function
+
+const taxAdd = function(rate, value){ // simple function not an arrow
+    return value + (value * rate)/100;
+}
+
+console.log(addTax(10, 200));
+
+
+const addVAT = addTax.bind(null, 23);
+console.log(addVAT(100));
+console.log(addVAT(200));
+
+const addTaxRate = function(rate){
+    return function (value){
+        return value + (value * rate)/100;
+    }
+}
+
+const addVAT2 = addTaxRate(23);
+console.log(addVAT2(100));
+console.log(addVAT2(200));
