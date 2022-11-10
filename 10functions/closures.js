4550763ac249a4aa9474d0b3198616906b2658d3
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-11-10 16:04:08 +0100

    10th section ended

--- 10functions/closures.js
+++ 10functions/closures.js
@@ -1,4 +1,5 @@
 'use strict';
+/*
 
 const secureBooking = function(){
     let passengerCount = 0;
@@ -17,4 +18,50 @@ booker();// 3 passengers
 
 // !!!! ----- We don not have to manually create closures, This is a JS feature that happens autmatically 
 
+ */
 
+// first example Closures
+let f;
+
+const g = function(){//function expression
+    const a = 23;
+    f = function(){
+        console.log(a * 2);
+    }
+}
+
+const h = function(){
+    const b = 777;
+    f = function(){
+        console.log(b * 2);
+    };
+}
+g();
+f();
+
+// Re-assigning f function
+h();
+f(); 
+
+//  Example 2
+const boardPassengers = function(n, wait){
+    const perGroup = n/3;
+
+    setTimeout(function(){
+        console.log(`We are now boarding all ${n} personal `);
+        console.log(`There are 3 groups, each with ${perGroup} passengers`)
+    }, wait * 1000); 
+
+    console.log(`Will Start boarding in ${wait} seconds `);
+};
+
+boardPassengers(180, 3);
+
+(function(){
+    const header = document.querySelector('h1');
+    header.style.color = 'red';
+
+    document.querySelector('body').addEventListener('click', function(){
+        header.style.color = 'blue ';  
+    });
+})(); 
