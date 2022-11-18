7ab0151e357e47dd250b328428ddc3fb9f389a31
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-11-18 09:22:20 +0100

    Challenge 2

--- OOP/es6.js
+++ OOP/es6.js
@@ -110,6 +110,38 @@ const dan = Object.create(PersonProto);
 dan.init('Dan Lok', 1987);
 dan.calcAge();
 
+console.log('================Chellenge 2 in ES 6====================');
 
+class CarCl {
+    constructor(make, speed){
+        this.make = make;
+        this.speed = speed;
+    }
 
+    accelerate(){
+        this.speed +=10;
+        console.log(`${this.make} is going at ${this.speed} km/h`);
+    }
 
+    brake(){
+        this.speed -=5;
+        console.log(`${this.make} is going at ${this.speed} km/h`);    
+    }
+
+    get speedUS(){
+        return this.speed /1.6;
+    }
+
+    set speedUS(speed){
+        this.speed = speed * 1.6;
+    }
+}
+
+const ford = new CarCl('Ford', 120);
+console.log(ford.speedUS);
+ford.accelerate();
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+ford.speedUS = 50; 
+console.log(ford);
